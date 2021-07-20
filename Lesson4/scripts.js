2a565dc1ba261aa131af4b4a080190b649190e82
elemaye <elethington@gmail.com>
2021-07-20 13:25:16 -0700

    current date

--- Lesson4/scripts.js
+++ Lesson4/scripts.js
@@ -4,5 +4,11 @@ function toggleMenu() {
   document.getElementById("last-update").innerHTML = "Last Updated: " + document.lastModified;
   var date = new Date();
   document.getElementById("get-year").innerHTML = date.getFullYear();
-  document.getElementById("get-dow").innerHTML = date.getDay();
-  
+  let daynames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let d = new Date();
+  let dayname = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let year = d.getFullYear();
+  let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+  document.getElementById("currentdate").textContent = fulldate;
