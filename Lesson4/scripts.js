115d26f1227319c6071a43b068aeecea20d1b412
elemaye <elethington@gmail.com>
2021-07-20 13:55:08 -0700

    glitch

--- Lesson4/scripts.js
+++ Lesson4/scripts.js
@@ -1,14 +1,11 @@
-function toggleMenu() {
-    document.getElementById("main-nav").classList.toggle("hide");
-  }
-  document.getElementById("last-update").innerHTML = "Last Updated: " + document.lastModified;
-  var date = new Date();
-  document.getElementById("get-year").innerHTML = date.getFullYear();
+document.getElementById("last-update").innerHTML = "Last Updated: " + document.lastModified;
+var date = new Date();
+document.getElementById("get-year").innerHTML = date.getFullYear();
 
   let daynames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let d = new Date();
-  let dayname = daynames[d.getDay()];
+  let dayName = daynames[d.getDay()];
   let monthName = months[d.getMonth()];
   let year = d.getFullYear();
   let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
